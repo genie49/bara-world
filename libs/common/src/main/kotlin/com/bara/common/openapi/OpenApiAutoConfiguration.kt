a0c5ecc228fe6b38f2e106b5cb37be25f3506dfb
genie49 <kimgenie0409@gmail.com>
2026-04-07 23:43:44 +0900

    feat(infra): add Bearer JWT security scheme to Swagger UI

--- libs/common/src/main/kotlin/com/bara/common/openapi/OpenApiAutoConfiguration.kt
+++ libs/common/src/main/kotlin/com/bara/common/openapi/OpenApiAutoConfiguration.kt
@@ -1,7 +1,10 @@
 package com.bara.common.openapi
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -14,10 +17,21 @@ class OpenApiAutoConfiguration {
 
     @Bean
     fun openApi(props: OpenApiProperties): OpenAPI =
-        OpenAPI().info(
-            Info()
-                .title(props.title)
-                .version(props.version)
-                .description(props.description)
-        )
+        OpenAPI()
+            .info(
+                Info()
+                    .title(props.title)
+                    .version(props.version)
+                    .description(props.description)
+            )
+            .components(
+                Components().addSecuritySchemes(
+                    "bearer",
+                    SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+            .addSecurityItem(SecurityRequirement().addList("bearer"))
 }
