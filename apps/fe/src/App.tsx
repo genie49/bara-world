9e177aaec8530134de1663fd6207dc6f4801aa51
genie49 <kimgenie0409@gmail.com>
2026-04-07 20:06:01 +0900

    feat(fe): add shared layout with header navigation

--- apps/fe/src/App.tsx
+++ apps/fe/src/App.tsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import CallbackPage from './pages/CallbackPage'
+import Layout from './components/Layout'
 import MePage from './pages/MePage'
+import ProviderPage from './pages/ProviderPage'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/auth/callback" element={<CallbackPage />} />
-      <Route path="/me" element={<MePage />} />
+      <Route element={<Layout />}>
+        <Route path="/me" element={<MePage />} />
+        <Route path="/provider" element={<ProviderPage />} />
+      </Route>
     </Routes>
   )
 }
---
+++ apps/fe/src/pages/ProviderPage.tsx
@@ -0,0 +1,3 @@
+export default function ProviderPage() {
+  return <div>Provider 설정 (준비 중)</div>
+}
